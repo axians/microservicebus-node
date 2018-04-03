d60d108a4fae2cd1f8d4196e738deaae0c028285
Stefan Nilsson <stefannilsson@users.noreply.github.com>
2018-04-03 10:58:05 +0200

    Typo

--- start.js
+++ start.js
@@ -37,7 +37,7 @@ var os = require('os');
 var async = require('async');
 
 process.on('unhandledRejection', err => {
-    console.log("SERIOUS ERROR: Caught unhandledRejection. %o", err);
+    console.log("SERIOUS ERROR: Caught unhandledRejection. ", err);
 });
 var microServiceBusHost;
 var maxWidth = 75;

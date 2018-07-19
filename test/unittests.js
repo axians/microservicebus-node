23e50cbbe5e95adba19733c650999b98adf31565
Mikael Håkansson <wmmihaa@hotmail.com>
2018-07-19 11:21:14 +0200

    Added installl core to test

--- test/unittests.js
+++ test/unittests.js
@@ -27,7 +27,7 @@ var microServiceBusHost;
 
 describe('mSB should work', function(){
     it('padRight should work', function (done) {
-        util = require("../node_modules/microservicebus-core/lib/utils.js");
+        util = require("../lib/Utils.js");
         var t = util.padRight("microServiceBus.com", 25, ' ');
         expect(t).to.equal("microServiceBus.com      ");
         done();
@@ -52,6 +52,34 @@ describe('mSB should work', function(){
         settingsHelper.save();
         done();
     });
+    it('Removing microServiceBus-core should work', function (done) {
+        this.timeout(60000);
+        util.removeNpmPackage("microservicebus-core",  function (err) {
+            if (err) {
+                console.log("Unable to remove core".bgRed.white);
+                console.log("Error: " + err);
+                throw err;
+            }
+            else {
+                console.log("Core uninstalled successfully".bgGreen.white);
+                done();
+            }
+        });
+    });
+    it('Downloading microServiceBus-core should work', function (done) {
+        this.timeout(120000);
+        util.addNpmPackage("microservicebus-core@latest", true, function (err) {
+            if (err) {
+                console.log("Unable to install core update".bgRed.white);
+                console.log("Error: " + err);
+                throw err;
+            }
+            else {
+                console.log("Core installed successfully".bgGreen.white);
+                done();
+            }
+        });
+    });
     it('Create microServiceBus Node should work', function (done) {
         loggedInComplete1 = false;
         MicroServiceBusHost = require("../node_modules/microservicebus-core/lib/MicroServiceBusHost.js");
@@ -60,250 +88,3 @@ describe('mSB should work', function(){
         done();
     });
 });
-
-/*
-describe('Util functions', function () {
-    
-    it('padRight should work', function (done) {
-
-        var t = util.padRight("microServiceBus.com", 25, ' ');
-        expect(t).to.equal("microServiceBus.com      ");
-        done();
-    });
-    it('padLeft should work', function (done) {
-
-        var t = util.padLeft("microServiceBus.com", 25, ' ');
-        expect(t).to.equal("      microServiceBus.com");
-        done();
-    });
-    it('addNpmPackage (msbcam) should work', function (done) {
-        this.timeout(30000);
-        util.addNpmPackage("msbcam", function (err) {
-            expect(err).to.equal(null);   
-            done();
-        });
-    });
-    it('compare same version should work', function (done) {
-
-        var r = util.compareVersion("1.0.0", "1.0.0")
-        expect(r).to.equal(0);
-        done();
-    });
-    it('compare greater version should work', function (done) {
-
-        var r = util.compareVersion("1.0.0", "1.0.1")
-        expect(r).to.equal(-1);
-        done();
-    });
-    it('compare earlier version should work', function (done) {
-
-        var r = util.compareVersion("1.0.2", "1.0.1")
-        expect(r).to.equal(1);
-        done();
-    });
-    
-});
-
-describe('Encryption/Decryption', function () {
-    var dataToEncrypt = "Some data";
-    var encryptedBuffer;
-
-    it('Encryption should work', function (done) {
-
-        var dataToEncrypt = "Some data";
-        encryptedBuffer = util.encrypt(new Buffer(dataToEncrypt), "secret");
-        done();
-    });
-    it('Decryption should work', function (done) {
-
-        var decryptedBuffer = util.decrypt(encryptedBuffer, "secret");
-        var str = decryptedBuffer.toString('utf8');
-
-        if (str != dataToEncrypt)
-            throw "Encryption/Decryption failed";
-
-        done();
-    });
-});
-
-describe('Check configuration', function () {
-    it('ENV organizationId should be set', function (done) {
-        orgId = process.env.organizationId;
-        nodeKey = process.env.nodeKey;
-        expect(orgId).to.not.be.null;
-
-        done();
-    });
-    it('ENV nodeKey should be set', function (done) {
-        nodeKey = process.env.nodeKey;
-        expect(nodeKey).to.not.be.null;
-
-        done();
-    });
-});
-
-describe('Sign in', function () {
-    it('Save settings should work', function (done) {
-        settings = {
-            "hubUri": "wss://stage.microservicebus.com",
-            "trackMemoryUsage": 0,
-            "enableKeyPress": false,
-            "useEncryption": false,
-            "log": "",
-            "nodeName": "unitTestNode1",
-            "organizationId": process.env.organizationid,
-            "machineName": "unitTestNode1",
-            "id": "644424d1-b591-4fd0-b7c2-29736b2f51ac",
-            "sas": process.env.nodeKey,
-            "port": 9090
-        };
-        settingsHelper.settings = settings;
-        settingsHelper.save();
-        //util.saveSettings(settings);
-        done();
-    });
-    it('Create microServiceBus Node should work', function (done) {
-        loggedInComplete1 = false;
-        microServiceBusHost = new MicroServiceBusHost(settingsHelper);
-        expect(microServiceBusHost).to.not.be.null;
-        done();
-    });
-    it('Sign in should work', function (done) {
-        this.timeout(60000);
-        microServiceBusHost.OnStarted(function (loadedCount, exceptionCount) {
-            expect(exceptionCount).to.eql(0);
-            expect(loadedCount).to.eql(1);
-            done();
-        });
-        microServiceBusHost.OnStopped(function () {
-
-        });
-        microServiceBusHost.OnUpdatedItineraryComplete(function () {
-
-        });
-        try {
-            microServiceBusHost.Start();
-
-        }
-        catch (er) {
-            expect(err).to.be.null;
-            done();
-        }
-
-    });
-    it('Ping should work', function (done) {
-        this.timeout(60000);
-        var r = microServiceBusHost.TestOnPing("test");
-        expect(r).to.equal(true);
-        done();
-    });
-    it('Change Debug state should work', function (done) {
-        this.timeout(60000);
-        var r = microServiceBusHost.TestOnChangeDebug(true);
-        expect(r).to.equal(true);
-        done();
-    });
-});
-
-describe('Post Signin', function () {
-    it('azureApiAppInboundService.js should exist after login', function (done) {
-        var filePath = path.resolve(SCRIPTFOLDER, "azureApiAppInboundService.js");
-        var ret = fs.statSync(filePath);
-        ret.should.be.type('object');
-
-        done();
-    });
-    it('calling test should work', function (done) {
-        this.timeout(5000);
-        var url = 'http://localhost:9090';
-
-        request(url)
-            .get('/api/data/azureApiAppInboundService1/test')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200)//Status code
-            .end(function (err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.body.should.have.property('result');
-                res.body.result.should.equal(true);
-                console.log("GET Complete");
-                //done();
-                request(url)
-                    .delete('/api/data/azureApiAppInboundService1/test')
-                    .expect('Content-Type', 'application/json; charset=utf-8')
-                    .expect(200)//Status code
-                    .end(function (err, res) {
-                        if (err) {
-                            throw err;
-                        }
-                        res.body.should.have.property('result');
-                        res.body.result.should.equal(true);
-                        console.log("DELETE Complete");
-                        request(url)
-                            .post('/api/data/azureApiAppInboundService1/test')
-                            .send({ name: 'Manny', species: 'cat' })
-                            .expect('Content-Type', /json/)
-                            .expect(200)//Status code
-                            .end(function (err, res) {
-                                if (err) {
-                                    throw err;
-                                }
-                                res.body.should.have.property('result');
-                                res.body.result.should.equal(true);
-                                console.log("POST Complete");
-                                request(url)
-                                    .put('/api/data/azureApiAppInboundService1/test')
-                                    .send({ name: 'Manny', species: 'cat' })
-                                    .expect('Content-Type', /json/)
-                                    .expect(200)//Status code
-                                    .end(function (err, res) {
-                                        if (err) {
-                                            throw err;
-                                        }
-                                        res.body.should.have.property('result');
-                                        res.body.result.should.equal(true);
-                                        console.log("PUT Complete");
-                                        done();
-                                    });
-                            });
-                    });
-            });
-    });
-    it('javascriptaction.js should exist after calling service', function (done) {
-        var filePath = path.resolve(__dirname, SCRIPTFOLDER, "javascriptaction.js");
-
-        var ret = fs.statSync(filePath);
-        ret.should.be.type('object');
-        done();
-    });
-    it('ping should work', function (done) {
-        var pingResponse = microServiceBusHost.TestOnPing("");
-        pingResponse.should.equal(true);
-        done();
-    });
-    it('change debug state should work', function (done) {
-        var TestOnChangeDebugResponse = microServiceBusHost.TestOnChangeDebug(false);
-        TestOnChangeDebugResponse.should.equal(true);
-        done();
-    });
-    it('change state should work', function (done) {
-        var TestOnChangeDebugResponse = microServiceBusHost.TestOnChangeState("Stop");
-        done();
-    });
-    it('removeNpmPackage (msbcam) should work', function (done) {
-        this.timeout(30000);
-        util.removeNpmPackage("msbcam", function (err) {
-            expect(err).to.be.null;
-            done();
-        });
-    });
-    it('Sign out should work', function (done) {
-        //process.exit(99);
-        //microServiceBusHost.TestStop();
-        done();
-        setTimeout(function(){process.exit(99);},1000);
-    });
-}); 
-
-*/
